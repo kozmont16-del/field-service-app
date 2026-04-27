6d91f443b6d1ef469aea86c207b72866fff6f8ee
kozmont16-del <kozmont16@gmail.com>
2026-04-27 18:16:33 +0900

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,10 @@
 
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
