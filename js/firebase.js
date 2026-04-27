55f1e33ab15c5bec9a878b70bd51d881ea1e3569
kozmont16-del <kozmont16@gmail.com>
2026-04-27 18:36:25 +0900

    Add storageBucket and messagingSenderId to config

--- js/firebase.js
+++ js/firebase.js
@@ -3,6 +3,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyCd1bFPWT-P-aLRzzbDV3CFo1vJg4XHS9o",
   authDomain: "zone-967f4.firebaseapp.com",
   projectId: "zone-967f4",
+storageBucket: "zone-967f4.firebasestorage.app",
+  messagingSenderId: "147792254614",
+  appId: "1:147792254614:web:f320ce2a17ad8e945bd4b7",
+  measurementId: "G-409LYRBJZX"
 };
 
 firebase.initializeApp(firebaseConfig);
